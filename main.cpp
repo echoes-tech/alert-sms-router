676249993ffa23188e0a4c66bf0d3ccf29556277
Thomas <thomas@echoes.fr>
2017-02-27 16:50:58 +0100

    opensourcing : changing headers for cpp files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
-/* 
- * File:   main.cpp
- * Author: cde
+/**
+ * @file main.cpp
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
  *
- * Created on 21 mai 2015, 15:33
+ * Alert - SMS Router is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * 
  */
 
 #include <cstdlib>
@@ -69,4 +87,3 @@ int main(int argc, char** argv)
         }
     return res;
 }
-
--- src/SavedSend.cpp
+++ src/SavedSend.cpp
@@ -1,8 +1,26 @@
-/* 
- * File:   SavedSend.cpp
- * Author: cde
+/**
+ * @file SavedSend.cpp
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Alert - SMS Router is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  * 
- * Created on 26 mai 2015, 14:30
  */
 
 #include "SavedSend.h"
@@ -23,4 +41,3 @@ SavedSend::SavedSend(const SavedSend& orig) {
 
 SavedSend::~SavedSend() {
 }
-
